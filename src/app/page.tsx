d3fa3ca2b470aff707b7d1543ee4ebb1f61fe67a
DungDepTrai <dung.nn193209@sis.hust.edu.vn>
2024-07-19 09:44:26 +0700

    Refactor CSS and update page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center w-screen h-screen">
-            <h2>Next.js</h2>
+            <h2>Todo App</h2>
         </div>
     );
 }
